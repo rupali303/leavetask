2aad9bd13dd9573c6026c10d5e6c5bd3eedcb581
rupali patil <rupalishewale2010@gmail.com>
2023-08-18 18:30:49 +0530

    work on it

---
+++ src/TaskLeave/Main.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import {RouterProvider} from 'react-router-dom'
+import { router } from './router/router'
+function Main() {
+  return (
+    <div>
+        <RouterProvider router={router}/>
+    </div>
+  )
+}
+
+export default Main
